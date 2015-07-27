6abc3ea68e41b0c5bfd6d34898eb83078f52e054
Greg Domorski <greg.domorski@gmail.com>
2015-07-27 05:26:36 -0400

    Angular Update

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -2,11 +2,11 @@
   
   controllers.PlayerController = function ($scope){
     $scope.players = [ 
-    {name: 'Lebron James', team: 'Cleveland Cavaliers', position: 'Forward'},
-    {name:'Stephen Curry', team: 'Golden State Warriors', position: 'Point Guard'},
-    {name:'Dwight Howard', team: 'Houston Rockets', position: 'Center/Power Forward'},
-    {name:'Kobe Bryant', team: 'Los Angeles Lakers', position: 'Guard'}, 
-    {name:'Damian Lillard', team: 'Portland Trailblazers', position: 'Point Guard'}
+    {name: 'Lebron James', team: 'Cleveland Cavaliers', position: 'Forward', points: '25.3'},
+    {name:'Stephen Curry', team: 'Golden State Warriors', position: 'Point Guard', points: '23.8'},
+    {name:'Dwight Howard', team: 'Houston Rockets', position: 'Center/Power Forward', points: '15.8'},
+    {name:'Kobe Bryant', team: 'Los Angeles Lakers', position: 'Guard', points: '22.3'}, 
+    {name:'Damian Lillard', team: 'Portland Trailblazers', position: 'Point Guard', points: '21.0'}
    
     ];
   }
