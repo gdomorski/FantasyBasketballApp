80a5584f1a5ef5bb7253552c9e7970e833c7e132
Greg Domorski <greg.domorski@gmail.com>
2015-07-26 21:19:02 -0400

    Update Loader

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -4,7 +4,9 @@
     $scope.players = [ 
     {name: 'Lebron James', team: 'Cleveland Cavaliers', position: 'Forward'},
     {name:'Stephen Curry', team: 'Golden State Warriors', position: 'Point Guard'},
-    {name:'Dwight Howard', team: 'Houston Rockets', position: 'Center/Power Forward'}, 
+    {name:'Dwight Howard', team: 'Houston Rockets', position: 'Center/Power Forward'},
+    {name:'Kobe Bryant', team: 'Los Angeles Lakers', position: 'Guard'}, 
+    {name:'Damian Lillard', team: 'Portland Trailblazers', position: 'Point Guard'}
    
     ];
   }
